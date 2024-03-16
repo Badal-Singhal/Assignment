85ecd619650da33087353bf7b0dae17983498e5b
Badal-Singhal <146357764+Badal-Singhal@users.noreply.github.com>
2024-03-16 09:13:43 -0700

    HashRouter

--- src/Components/UserAuthentication.jsx
+++ src/Components/UserAuthentication.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  HashRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./MainLayout";
 import Signin from "../pages/Signin";
 import Joinin from "../pages/Joinin";
 
 export default function UserAuthentication() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Signin />} />
         <Route path="/Joinin" element={<Joinin />} />
       </Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
